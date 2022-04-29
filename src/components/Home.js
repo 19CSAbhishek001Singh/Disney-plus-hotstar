dabe65cebe830dc7b02c7c5bbd46c753ec14ab61
Abhishek <abhisheksinghcsanand@gmail.com>
2022-04-29 19:21:15 +0530

    done will learn firebase and other things later

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,9 @@
-import React,{useEffect} from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import ImgSlider from '../components/ImgSlider'
 import Viewers from "./Viewers"
 import Movies from "./Movies"
-import db from '../firebase'
 function Home() {
-useEffect(() => {
-db.collection('movies').onSnapshot((snapshot) => {
-let tempMovies=snapshot.docs.map((doc)=>{
-return {id: doc.id, ...doc.data()}
-})
-})
-}, []);
   return (
     <Container>
       <ImgSlider />
--- src/features/movie/movieSlice.js
+++ src/features/movie/movieSlice.js
@@ -1,1 +1,19 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  movies: []
+}
+// Actions call reducers and reducers change the state
+const movieSlice = createSlice({
+  name: "movie",
+  initialState,
+  reducers: {
+    setMovies: (state, action) => {
+      state.movies = action.payload;
+    }
+  }
+})
+
+export const { setMovies } = movieSlice.actions;
+export const selectMovies = (state) => state.movie.movies;
+export default movieSlice.reducer;
